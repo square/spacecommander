8fb5001145bac0a98c480ca3f8c1108d98978e53
Robert Gilliam <rhg@squareup.com>
2015-06-24 15:45:17 -0700

    Add tests. Add comment exemption to dry run.

---
+++ Testing Support/ExemptViaCommentFormattedExample.m
@@ -0,0 +1,58 @@
+// MARK: Formatter Exempt
+// Copyright 2015 Square, Inc
+
+#import @"XYZGeometry.h"
+#import "Blah.h"
+#import <Great.h>
+
+
+#define RKTAddTestCaseCategoryInterface(klass, name) \
+@class klass; \
+@interface KIFTestCase (klass##_ConvenienceAdditions) \
+@property (nonatomic, readonly, strong) klass *name; \
+@end
+
+@interface ProblemChild<NeatInterface>
+@property(nonatomic, assign, getter = isWackSpacingGetter, readonly) BOOL wackSpacing;
+
+- (void)performOnMainThreadUsingBlock:(dispatch_block_t)block SQ_DEPRECATED("Should use addOperationWithBlock: on NSOperationQueue's mainQueue. Will be removed after 3/1/2015.");
+@end
+
+// The below interface should have its prepended newlines left alone due to this comment
+@interface Foo
+@end
+
+// The inline constructors here are problematic and will get a semicolon added unless we put them on a single line.
+struct Update {
+	UpdateType type;
+	string value;
+	Update()
+		: type(UPDATE_WRITE), value("") {
+	}
+	Update(UpdateType t, const Slice &v)
+		: type(t), value(v.data(), v.size()) {
+	}
+};
+
+/* Same deal here, don't mess with adding newlines after this comment block 
+ * with a different comment style 
+ * and trailing spaces */  
+@implementation Foo
+@end
+
+@implementation ProblemChild	
+
+- (void)methodWithParams:(NSString*)param1 another:(BOOL )param2
+{
+	SQSelfSelector1(_windowDidResignKeyNotification:);
+        NSString *keys = [NSSet setWithArray:@[
+                        SQTypedKeyPath(MQItemControl, highlighted),
+                        SQTypedKeyPath(MQItemControl, enabled),
+                        SQTypedKeyPath(MQItemControl, selected)
+                                             ]];
+	NSArray* testLiteral = @[cool];
+	NSDictionary* dictLiteral = @{ @"foo": testLiteral };
+	SQCheckCondition(NO,, @"Will the commas stay together?");
+}
+
+@end
--- format-objc-file-dry-run.sh
+++ format-objc-file-dry-run.sh
@@ -6,10 +6,11 @@
 export CDPATH=""
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# "#pragma Formatter Exempt" on the first line means don't format this file 
-line="$(head -1 "$1" | xargs)" # (read the first line and trim it)
-if [ "$line" == "#pragma Formatter Exempt" ]; then
-	cat "$1" && exit 0
+# "#pragma Formatter Exempt" or "// MARK: Formatter Exempt" means don't format this file.
+# Read the first line and trim it.
+line="$(head -1 "$1" | xargs)" 
+if [ "$line" == "#pragma Formatter Exempt" -o "$line" == "// MARK: Formatter Exempt" ]; then
+  cat "$1" && exit 0
 fi
 
 cat "$1" | \
