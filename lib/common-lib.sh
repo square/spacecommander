f6ae293361492ace77cd7de6cc8443dc3f73c561
Kent Sutherland <git@ksuther.com>
2015-08-31 11:31:37 -0600

    Added additional comments about .formatting-directory to common-lib.sh. Converted spaces to tabs. Updated README.

--- lib/common-lib.sh
+++ lib/common-lib.sh
@@ -8,11 +8,12 @@ IFS=$'\n'
 # If the repo contains a .formatting-directory file, only files in the specified directories will be returned (one directory per line).
 # If .formatting-directory doesn't exist then all files in the repository are checked.
 function directories_to_check() {
-    locations_to_diff=''
+	locations_to_diff=''
 	[ -e ".formatting-directory" ] && locations_to_diff=$( cat .formatting-directory | sed 's/ /\\ /g' );
 }
 
 # Returns a list of Objective-C files to format.
+# If .formatting-directory exists, then only directories specified in .formatting-directory will be included (see directories_to_check).
 # No parameters: return staged ObjC files only
 # Optional parameter: a git SHA. Returns a list of all ObjC files which have changed since that SHA
 function objc_files_to_format() {
@@ -24,6 +25,7 @@ function objc_files_to_format() {
 }
 
 # Returns a list of all Objective-C files in the git repository.
+# If .formatting-directory exists, then only directories specified in .formatting-directory will be included (see directories_to_check).
 function all_valid_objc_files_in_repo() {
 	directories_to_check
 	files=$(git ls-tree --name-only --full-tree -r HEAD -- $locations_to_diff | grep -e '\.m$' -e '\.mm$' -e '\.h$' -e '\.hh$')
