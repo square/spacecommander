c779c0755d06b3bf3a7b7de1348163a2acf29d78
Anup Doshi <anupaug17@gmail.com>
2016-03-22 12:07:12 -0400

    Add directories to ignore

--- lib/common-lib.sh
+++ lib/common-lib.sh
@@ -12,8 +12,16 @@ function directories_to_check() {
 	[ -e ".formatting-directory" ] && locations_to_diff=$( cat .formatting-directory );
 }
 
+# If the repo contains a .formatting-directory-ignore file, files in the specified directories will be ignored (one directory per line).
+function directories_to_ignore() {
+	if [ -f ".formatting-directory-ignore" ]; then
+		files=$(echo "$files" | grep -v -f .formatting-directory-ignore)
+	fi
+}
+
 # Returns a list of Objective-C files to format.
 # If .formatting-directory exists, then only directories specified in .formatting-directory will be included (see directories_to_check).
+# If .formatting-directory-ignore exists, then directories specified in .formatting-directory-ignore will be excluded (see directories_to_ignore).
 # No parameters: return staged ObjC files only
 # Optional parameter: a git SHA. Returns a list of all ObjC files which have changed since that SHA
 function objc_files_to_format() {
@@ -21,13 +29,16 @@ function objc_files_to_format() {
 	directories_to_check
 	# optional_base_sha is intentionally unescaped so that it will not appear as empty quotes.
 	files=$(git diff --cached --name-only $optional_base_sha --diff-filter=ACM -- $locations_to_diff | grep -e '\.m$' -e '\.mm$' -e '\.h$' -e '\.hh$')
+	directories_to_ignore
 	echo "$files" | grep -v 'Pods/' | grep -v 'Carthage/' >&1
 }
 
 # Returns a list of all Objective-C files in the git repository.
 # If .formatting-directory exists, then only directories specified in .formatting-directory will be included (see directories_to_check).
+# If .formatting-directory-ignore exists, then directories specified in .formatting-directory-ignore will be excluded (see directories_to_ignore). 
 function all_valid_objc_files_in_repo() {
 	directories_to_check
 	files=$(git ls-tree --name-only --full-tree -r HEAD -- $locations_to_diff | grep -e '\.m$' -e '\.mm$' -e '\.h$' -e '\.hh$')
+	directories_to_ignore
 	echo "$files" | grep -v 'Pods/' | grep -v 'Carthage/' >&1
 }
