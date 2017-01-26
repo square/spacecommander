94c61510954921ea71e8a000ba7ad553ad46e4ad
Don Gaxho <dgaxho@thelevelup.com>
2017-01-25 20:51:46 -0500

    Ensure setup script is run before formatting with Xcode services

--- spacecommander-oclint-setup.sh
+++ spacecommander-oclint-setup.sh
@@ -17,5 +17,8 @@ fi
 export MASTER_COMMIT_HASH=$(git rev-parse origin/master)
 source Pods/SpaceCommander/lib/common-lib.sh
 export CHANGED_FILES_STRING=$(objc_files_to_format $MASTER_COMMIT_HASH)
+SETUP_COMPLETE_PATH="./Pods/SpaceCommander/.setup_complete.txt"
+touch $SETUP_COMPLETE_PATH
+echo "true" > $SETUP_COMPLETE_PATH
 echo '#### Done                 #####'
 echo '###############################'
