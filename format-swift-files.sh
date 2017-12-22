4ab3025d564c4c05aa3c097d404675e47b3c17bc
b-cootner <b.cootner@thelevelup.com>
2017-12-22 14:58:31 -0500

    [Enhancement] LU-17104 Add swiftlint rules for UnitTests

--- format-swift-files.sh
+++ format-swift-files.sh
@@ -7,4 +7,5 @@ SWIFT_LINT_DIR="/Pods/SwiftLint/swiftlint"
 SWIFTLINT="${DIR/$SC_DIR/$SWIFT_LINT_DIR}"
 REPO_PATH="${DIR%$SC_DIR}"
 
-$SWIFTLINT autocorrect --path $REPO_PATH
+$SWIFTLINT autocorrect --path $REPO_PATH --config .swiftlint.yml
+$SWIFTLINT autocorrect --path $REPO_PATH --config .swiftlint-unit-tests.yml
--- swiftlint-setup.sh
+++ swiftlint-setup.sh
@@ -14,7 +14,13 @@ echo "SwiftLint version $($SWIFTLINT version)"
 if [ -e $REPO_PATH/.swiftlint.yml ]; then
   cp $REPO_PATH/.swiftlint.yml $DIR/.swiftlint.yml
 else
-  echo "No .swiftlint.yml rules file in repo. Swiftlint will be using the default rules!"
+  echo "Error: No .swiftlint.yml rules file in repo."
+fi
+
+if [ -e $REPO_PATH/.swiftlint-unit-tests.yml ]; then
+  cp $REPO_PATH/.swiftlint-unit-tests.yml $DIR/.swiftlint-unit-tests.yml
+else
+  echo "Error: No .swiftlint-unit-tests.yml rules file in repo."
 fi
 echo '#### Done                 #####'
 echo '###############################'
